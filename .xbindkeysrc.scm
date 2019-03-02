c50aa5cbba0e1aaac36c4295e99779d075118ee2
Brian Stamper <brianstamper10+github@gmail.com>
2019-03-02 12:06:21 -0500

    Change to Guile based xbindkeys config

---
+++ .xbindkeysrc.scm
@@ -0,0 +1,4 @@
+
+(xbindkey '("c:66") "xdotool mousedown 1")
+(xbindkey '(release "c:66") "xdotool mouseup 1")
+
--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/dotfiles/.vim/bundle/Vundle
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.xbindkeysrc ~/.xbindkeysrc
+ln -sf ~/dotfiles/.xbindkeysrc.scm ~/.xbindkeysrc.scm
 
 vim +PluginInstall +qall
 
